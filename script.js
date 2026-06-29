07bfb8fbf3e52c91b3a7a905067184b786b855a5
karinecampos17-rgb <karine.campos17@escola.pr.gov.br>
2026-06-29 11:40:34 -0300

    1

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const avanca = document.querySelectorALL('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click' , function(){
       const atual = document.querySelector('.ativo');
-      const proximopasso
+      const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+      atual.classList.remove('ativo');
+      document.getElementById(proximoPasso).classList.add('ativo')
     })
 })
